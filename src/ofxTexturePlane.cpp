e6deb3370a6eb57fc647dcf88da7603b33defcf8
Andrew McWilliams <andy@jahya.net>
2015-06-13 16:03:09 -0400

    Initialises with zero offset

--- src/ofxTexturePlane.cpp
+++ src/ofxTexturePlane.cpp
@@ -5,4 +5,14 @@ void ofxTexturePlane::setup(string filename){
     image.loadImage(filename);
     texture = image.getTextureReference();
     texture.setTextureWrap(GL_MIRRORED_REPEAT, GL_MIRRORED_REPEAT);
+    setOffsetX(0);
+    setOffsetY(0);
+}
+
+void ofxTexturePlane::setOffsetX(float offsetX) {
+    tx0 = offsetX;
+}
+
+void ofxTexturePlane::setOffsetY(float offsetY) {
+    ty0 = offsetY;
 }
--- src/ofxTexturePlane.h
+++ src/ofxTexturePlane.h
@@ -6,7 +6,10 @@ class ofxTexturePlane  : public ofBaseApp{
 
 public:
     void setup(string filename);
+    void setOffsetX(float offsetX);
+    void setOffsetY(float offsetY);
 protected:
     ofImage image;
     ofTexture texture;
+    float tx0, ty0, tx1, ty1;
 };
