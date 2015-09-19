558eb6c6038f551f370892fd29b70a07e0ba0956
Andrew McWilliams <andy@jahya.net>
2015-09-19 10:15:22 -0400

    Simplifies example file

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -6,7 +6,7 @@
 //https://www.flickr.com/photos/garryknight/7651797988/in/photolist-cEasXw-qhnBmB-sujXPh-qEjktg-dV3zUH-dDpDbP-sfSEUm-sfZunV-pZWCXc-pZWDQz-sfZw8P-rTi2E5-pkBTcz-qhnB3v-dXtCsw-qWqcnE-aqaCep-fLxfdy-a6dghg-o57YmE-9yWbQp-faSZar-dqAeEF-o4mVQW-nhiwzN-jJbVVU-cpHhjj-kQ2UTZ-oDujpf-aWG7mc-bBZ6fw-9SwQCw-oGaFfs-kSnrqv-kAouvv-eujDe5-eAVNME-ofVKfT-oUrZVg-snfgJh-nF1U6b-pC53ar-gqjF2k-aJDhST-cpHiyj-rwbEGo-nwKi7N-rsiRbv-ecL35L-qmw2Ca
 
 void ofApp::setup(){
-    ofSetWindowShape(680, 744);
+    ofSetWindowShape(680, 680);
 
     seamlessPlane1.setup("seamless.jpg", 200, 200);
     buildingPlane1.setup("building.png", 200, 200);
@@ -22,20 +22,20 @@ void ofApp::setup(){
 }
 
 void ofApp::update(){
-    seamlessPlane1.incrementTextureOffsetX(0.001);
-    buildingPlane1.incrementTextureOffsetY(0.001);
-    landscapePlane1.incrementTextureOffsetX(0.001);
+    seamlessPlane1.incrementTextureOffsetX(0.003);
+    buildingPlane1.incrementTextureOffsetY(0.003);
+    landscapePlane1.incrementTextureOffsetX(0.003);
     
-    seamlessPlane2.setTextureOffsetX(mouseX * 0.001);
-    seamlessPlane2.setTextureOffsetY(mouseY * 0.001);
-    buildingPlane2.setTextureOffsetX(mouseX * 0.001);
-    buildingPlane2.setTextureOffsetY(mouseY * 0.001);
-    landscapePlane2.setTextureOffsetX(mouseX * 0.001);
-    landscapePlane2.setTextureOffsetY(mouseY * 0.001);
+    seamlessPlane2.setTextureOffsetX(mouseX * 0.003);
+    seamlessPlane2.setTextureOffsetY(mouseY * 0.003);
+    buildingPlane2.setTextureOffsetX(mouseX * 0.003);
+    buildingPlane2.setTextureOffsetY(mouseY * 0.003);
+    landscapePlane2.setTextureOffsetX(mouseX * 0.003);
+    landscapePlane2.setTextureOffsetY(mouseY * 0.003);
     
-    seamlessPlane3.incrementTextureOffsetX(-0.001);
-    buildingPlane3.incrementTextureOffsetY(-0.001);
-    landscapePlane3.incrementTextureOffsetX(-0.001);
+    seamlessPlane3.incrementTextureOffsetX(-0.003);
+    buildingPlane3.incrementTextureOffsetY(-0.003);
+    landscapePlane3.incrementTextureOffsetX(-0.003);
 }
 
 void ofApp::draw(){
@@ -54,7 +54,6 @@ void ofApp::draw(){
     landscapePlane3.draw(460, 460);
     
     ofSetColor(ofColor::black);
-    ofDrawBitmapString("Image credits:\nhttp://10ravens.deviantart.com/art/Seamless-wood-planks-texture-196937389\nhttps://www.flickr.com/photos/davelau/1061502581/in/photolist-2BNtJD-f9cyrp-4EWzrD-mf3ohG-6SL4K8-rpZhhi-cR2Y4-oRbHQ3-74HphA-axqy86-6oLop8-sfB4Rm-6vyv87-jweENe-pDokBh-pnLYyF-bUjwNe-nbj5x5-c2rYhC-5Keqs4-prZanK-naWAfw-byiPp8-AvNPk-4wp1yJ-ctBgSh-4SfP8f-4Jq1jU-7RFL1k-2UxfS4-cJtdjq-r1tp1d-6XpF6b-8tkzM8-oLG9TD-hjJG7Y-8jSwxn-qtjPtk-4KVpE5-bzuJjM-4RKuvi-p5eB82-edWUd7-dFGA8c-8FqgLP-naW9EA-gK7JC9-4VddEM-9vNY5B-dLzkef\nhttps://www.flickr.com/photos/garryknight/7651797988/in/photolist-cEasXw-qhnBmB-sujXPh-qEjktg-dV3zUH-dDpDbP-sfSEUm-sfZunV-pZWCXc-pZWDQz-sfZw8P-rTi2E5-pkBTcz-qhnB3v-dXtCsw-qWqcnE-aqaCep-fLxfdy-a6dghg-o57YmE-9yWbQp-faSZar-dqAeEF-o4mVQW-nhiwzN-jJbVVU-cpHhjj-kQ2UTZ-oDujpf-aWG7mc-bBZ6fw-9SwQCw-oGaFfs-kSnrqv-kAouvv-eujDe5-eAVNME-ofVKfT-oUrZVg-snfgJh-nF1U6b-pC53ar-gqjF2k-aJDhST-cpHiyj-rwbEGo-nwKi7N-rsiRbv-ecL35L-qmw2Ca", 20, 686);
 }
 
 void ofApp::keyPressed(int key){
